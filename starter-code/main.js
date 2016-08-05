558ebce7e43d9bf1029c71838f3737240d79f39f
wnamen <williamjosephnamen@gmail.com>
2016-08-05 11:01:10 -0400

    added loop to create board and cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,22 @@ var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
 
+if (cardOne === cardThree) {
+  alert("You found a match!");
+} else if (cardTwo === cardFour) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again!");
+}
+
+var gameBoard = document.getElementById('game-board');
+
+function createBoard() {
+
+  for (var i = 0; i < cards.length; i++) {
+    var cardEle = document.createElement('div');
+    cardEle.className = 'card';
+    gameBoard.appendChild(cardEle);
+  }
+}
+
