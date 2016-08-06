5e47eb8c7a4a37dc1164d24e2be576086fa3f249
wnamen <williamjosephnamen@gmail.com>
2016-08-06 12:47:02 -0400

    updated JS and added card imgs

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,43 @@
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
-
-if (cardOne === cardThree) {
-  alert("You found a match!");
-} else if (cardTwo === cardFour) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again!");
-}
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById('game-board');
+
 
 function createBoard() {
 
   for (var i = 0; i < cards.length; i++) {
     var cardEle = document.createElement('div');
+
     cardEle.className = 'card';
+    cardEle.setAttribute('data-card', cards[i]);
+    cardEle.addEventListener('click', isTwoCards);
+
     gameBoard.appendChild(cardEle);
   }
 }
+
+function isMatch(cards) {
+ (cards[0] === cards[1]) ? alert("You found a match!") : alert("Sorry, try again!");
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  console.log(push(this.getAttribute('data-card'));
+
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = "<img src='/home/wnamen/fundamentals/wdi-fundamentals-memorygame/starter-code/king_of_spades.png'>";
+  } else {
+    this.innerHTML = "<img src='/home/wnamen/fundamentals/wdi-fundamentals-memorygame/starter-code/queen_of_hearts.png'>";
+  }
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+
+}
+
+createBoard();
 
